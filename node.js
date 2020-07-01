f9aa79e7432f2352335c369dae7e7080aaf4d196
Louis SCHNEIDER <louis@schne.id>
2020-07-01 12:25:12 +0200

    add rules

--- node.js
+++ node.js
@@ -4,6 +4,7 @@ module.exports = {
   plugins: [],
   extends: ['./base.js', 'plugin:node/recommended-module'],
   rules: {
+    'no-underscore-dangle': 'off',
     'node/no-unsupported-features/es-syntax': 'off',
     'node/no-missing-import': 'off',
   },
